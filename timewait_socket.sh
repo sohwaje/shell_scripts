a3d4788ac498572fc266bc23c81df9b2a70934f8
sohwaje <kissenyo@gmail.com>
2019-05-24 23:51:44 +0900

    Update timewait_socket.sh

--- timewait_socket.sh
+++ timewait_socket.sh
@@ -7,5 +7,5 @@ alert=$(echo "$time_wait < $time_wait_idle" | bc)         # time_wait 개수와 
 if [ "$alert" -eq 1 ]; then
   date_str=$(date '+%Y/%m/%d %H:%H:%S')
   echo "[$date_str] TIME_WAIT 소켓 개수 : $time_wait 개"
-  echo "Alert 발생 서버:`hostname`"| mail -s "TIME_WAIT 소켓이 $time_wait 개입니다." $YOUR_EMAIL_ADDRESS
+  echo "Alert 발생 서버:`hostname`"| mail -s "TIME_WAIT 소켓이 $time_wait개 이상 생겨났습니다." $YOUR_EMAIL_ADDRESS
 fi
