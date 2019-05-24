538210989ace6c662302c5f322de5804850a1bd6
sohwaje <kissenyo@gmail.com>
2019-05-24 17:09:05 +0900

    시스템의 TIME_WAIT 소켓수가 임계치 이상 생겨나면 경고 메일을 발송한다.

---
+++ timewait_socket.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+time_wait_idle=20
+
+time_wait=$(netstat -napo | grep -i 'TIME_WAIT' | wc -l)
+
+alert=$(echo "$time_wait < $time_wait_idle" | bc)
+
+if [ "$alert" -eq 1 ]; then
+  date_str=$(date '+%Y/%m/%d %H:%H:%S')
+
+  echo "[$date_str] TIME_WAIT 소켓 개수 : $time_wait 개"
+  mail -s "웹하드의 TIME_WAIT 소켓이 $tim_wait 개입니다." $YOUR_EMAIL_ADDRESS
+fi
+exit 0
