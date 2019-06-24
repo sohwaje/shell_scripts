2535d4e0c936be016a8ff07938f04ba4e228097b
sohwaje <kissenyo@gmail.com>
2019-06-24 15:42:20 +0900

    Create longtime_no_see_file_delete.sh

---
+++ longtime_no_see_file_delete.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#[1] find 경로에서 1년 이상 변경이 없는 모든 파일을 찾아 삭제한다.
+#[2] 에러가 발생하지 않도록 -f옵션 사용, -v옵션을 사용하여 삭제한 파일명을 기록. 
+#[3] 필요 시 삭제하지 않고, 파일명을 기록하는 용도로 사용할 수 있다.
+TARGET_DIR="/home"
+
+# 삭제
+find $TARGET_DIR -name "*.*" -mtime +364 -print | xargs rm -fv
+
+# 아카이브
+#find $TARGET_DIR -name "*.*" -mtime +364 -print >> no_use_file.txt
