7ba4c1d285b3281c060fee3f8b1d1eed65c2fca4
sohwaje <kissenyo@gmail.com>
2020-08-14 23:00:20 +0900

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-#yum update -y
-#curl -s https://get.docker.com | sudo sh && systemctl start docker && systemctl enable docker
+sudo yum install -y httpd
 
-sudo yum install -y nginx
-sudo systemctl start nginx
