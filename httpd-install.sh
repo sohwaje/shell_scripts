8f23d8e268cdc97333818c8825b90298f77482c6
sohwaje <kissenyo@gmail.com>
2020-08-19 10:54:58 +0900

    Update httpd-install.sh

--- httpd-install.sh
+++ httpd-install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+sudo yum update -y
+sudo curl -s https://get.docker.com | sudo sh && systemctl start docker && systemctl enable docker
 sudo yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
+sudo sed -i 's/^Listen 80$/Listen 38080/' /etc/httpd/conf/httpd.conf
+sudo cat "Test-Page" > /var/www/html/index.html
+sudo systemctl start httpd
+sudo systemctl enable httpd
