62f9f9cf71b90c8e4e1ac483ceb2a7462f36f428
sigongweb <kisssulran@i-screammedia.com>
2020-09-14 10:28:34 +0900

    update

--- httpd-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo yum update -y
-sudo curl -s https://get.docker.com | sudo sh && systemctl start docker && systemctl enable docker
-sudo yum install -y httpd
-sudo sed -i 's/^Listen 80$/Listen 38080/' /etc/httpd/conf/httpd.conf
-sudo echo "Test-Page" > /var/www/html/index.html
-sudo systemctl start httpd
-sudo systemctl enable httpd
