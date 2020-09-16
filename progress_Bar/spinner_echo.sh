970e347a1e90fb7fdbf154d09a7cdfa84a8eb899
Lee Yusung <yusunglee@Leeui-iMac.local>
2020-09-16 21:39:08 +0900

    dd

--- progress_Bar/spinner_echo.sh
+++ progress_Bar/spinner_echo.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 clear
-spinner=( Ooooo oOooo ooOoo oooOo ooooO oooOo ooOoo oOooo);
-
-cat << EOF
-Hello.
-Thank you for trying this script out.
-I will now wait 10 seconds,
-but you will see a "spinner"
-as a visual for the user.
-EOF
-
+# spinner=( Ooooo oOooo ooOoo oooOo ooooO oooOo ooOoo oOooo);
+spinner=( '|' '/' '-' '\' )
 
 count(){
+  echo -en "\t Copying files"
   spin &
   pid=$!
 
@@ -19,19 +12,17 @@ count(){
   do
     sleep 1;
   done
-
-  kill $pid # 동작이 끝난 백그라운드 프로세스를 종료한다.
+  kill $pid
 }
 
 spin(){
   while [ 1 ]
   do
-    for i in ${spinner[@]};
+    for i in "${spinner[@]}"
     do
-      echo -ne "\r$i";
+      echo -ne "\r[$i]";
       sleep 0.2;
-    done;
+    done
   done
 }
-
 count
