22e8d28bb22cb9766fad136c43f2ac8bc10dbb67
Lee Yusung <yusunglee@Leeui-iMac.local>
2020-09-21 22:00:11 +0900

    update

--- function/make_dir.sh
+++ function/make_dir.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-
-function make_dirc
+make_home_dir()
 {
-  local dirc=$1
-  local content=$2
-  cd - >& /dev/null; cd $content; sudo mkdir ./$dirc
+if [[ -d /home/${1} ]];then
+  echo "/home/${1} directory does exist. Back up and recreate $1"
+  sudo mv /home/${1} /home/${1}-${date_}
+  sudo mkdir -p /home/${1} && sudo chmod 0755 /home/${1}
+else
+  echo -e "\e[0;31;47m /home/${1} directory does not exist. Create ${1} directory\e[0m"
+  sudo mkdir -p /home/${1} && sudo chmod 0755 /home/${1}
+fi
 }
-#usage
-make_dirc DIR_1
+
+# usage
+make_dir Test
