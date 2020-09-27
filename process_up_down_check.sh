227599fa205906fb35b4ac3433a1e1630f1887c8
Lee Yusung <yusunglee@Leeui-iMac.local>
2020-09-27 20:31:39 +0900

    add how to use sctips

--- process_up_down_check.sh
+++ process_up_down_check.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+################################################################################
+# Instead of using the process name to determine what to monitor, use the string
+# included in the cmdline. This scripts uses both cmdline and pid.
+# When the monitoring object is down, alert it to slack channel.
+# You have to use a unigue string, because match the pid exactly.
+## usage : once every minute. use a cron scheduler
+## */1 * * * * sh /home/azureuser/process_up_down_check.sh
+################################################################################
 proc_names=("instance01" "socket=/tmp/mysql.sock")
 date_str=$(date '+%Y/%m/%d %H:%M:%S')
-webhook=""
-
+webhook="Y o u r - S l a c k - W e b H o o K - U R L"
+fals_true_check_dir="/var/tmp/"
+# Alert to slack
 function slack_message(){
     # $1 : message
     # $2 : true=good, false=danger
@@ -16,27 +25,27 @@ function slack_message(){
     curl -s -d 'payload={"attachments":[{"color":"'"$COLOR"'","pretext":"<!channel> *lab*","text":"*HOST* : '"$HOSTNAME"' \n*MESSAGE* : '"$1"' '"$icon_emoji"'"}]}' $webhook > /dev/null 2>&1
 }
 
-# PID 찾기
+# Capture to Monitoring objects
 get_process()
 {
   local value=$(ps aux | grep $1 | grep -v grep | awk '{print $2}')
-  cat /proc/$value/cmdline | grep -ao $1 |head -1 2>/dev/null # standard out error
+  cat /proc/$value/cmdline | grep -ao $1 |head -1 2>/dev/null
 }
 
 # 1번
-if [[ -f /var/tmp/one.txt ]];then
-  echo ""
+if [[ -f $fals_true_check_dir/one.txt ]];then
+  return 0
 else
-  bash -c "cat << EOF > /var/tmp/one.txt
+  bash -c "cat << EOF > $fals_true_check_dir/one.txt
 false
 EOF"
 fi
 
 # 2번
-if [[ -f /var/tmp/two.txt ]];then
-  echo ""
+if [[ -f $fals_true_check_dir/two.txt ]];then
+  return 0
 else
-  bash -c "cat << EOF > /var/tmp/two.txt
+  bash -c "cat << EOF > $fals_true_check_dir/two.txt
 false
 EOF"
 fi
@@ -46,10 +55,10 @@ if [[ $(get_process ${proc_names[0]}) = ${proc_names[0]} ]] && [[ $(cat "/var/tm
 then
   sed -i 's/^false$/true/' /var/tmp/one.txt
   echo "$date_str : ${proc_names[0]} UP"
-  slack_message "process up"
+  slack_message "$date_str : ${proc_names[0]} up"
 elif [[ $(get_process ${proc_names[0]}) != ${proc_names[0]} ]] && [[ $(cat "/var/tmp/one.txt") == "true" ]]
 then
   sed -i 's/^true$/false/' /var/tmp/one.txt
   echo "$date_str : ${proc_names[0]} Down"
-  slack_message "process down" false
+  slack_message "$date_str : ${proc_names[0]} down" false # ":scream:"
 fi
