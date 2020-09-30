aa24376ba429ad05d116025f09c9ce39ddefe8d1
Lee Yusung <yusunglee@Leeui-iMac.local>
2020-09-30 22:47:10 +0900

    kill process with

--- processkill_with_name.sh
+++ processkill_with_name.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
-NAME=APP_NAME
-PID=$(ps -f | grep $NAME | grep -v grep | awk '{print $2}')
+var=$1
 
-if [ -z $PID ];then
-  echo "Process not found"
-else
-  echo "Kill Process $PID"
-  kill -9 $PID
+pid_status()
+{
+  ps -ef | grep $var | grep -v grep | awk '{print $2}' | head -1
+}
+
+checkpid()
+{
+  for i in $* ; do
+    [ -d "/proc/$i" ] && return 0
+  done
+  return 1
+}
+
+main()
+{
+  local pid=$(pid_status)
+  if checkpid $pid; then
+    echo "Kill Process $PID"
+    kill -9 $PID
+  else
+    echo "$pid is not...."
+  fi
+}
+
+if [ "$#" -ne 1 ]; then # 인자 1개만: not equal
+  echo "Usage: killproc [name]"
+  exit 0
 fi
+
+main
