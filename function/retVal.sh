899471a854086ac00c7d74e9181274e4f8ce4706
Lee Yusung <yusunglee@Leeui-iMac.local>
2020-10-02 18:54:49 +0900

    Check exit code

---
+++ function/retVal.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#### example 1
+retVal=$?
+_retVal()
+{
+  if [[ $retVal -eq 0 ]];then
+    echo "[OK]"
+    # do something
+  else
+    echo "[Failed]"
+    exit
+  fi
+}
+
+#### example 2
+retVal=$?
+test $retVal -eq 0 && echo "[OK]" || echo "[Failed]";
+exit $retVal
+
+#### example 3
+ec=$?
+case $ec in
+  0) ;;
+  1) echo "Command exited with no-zero"; exit 1;;
+  *) echo "something wrong exited code"; exit;;
+esac
