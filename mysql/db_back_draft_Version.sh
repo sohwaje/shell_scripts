535eaece4c258ed7b7a2b1e2896d95865d90cda2
sigongweb <kisssulran@i-screammedia.com>
2020-10-07 17:29:57 +0900

    update

--- mysql/db_back_draft_Version.sh
+++ mysql/db_back_draft_Version.sh
@@ -39,7 +39,7 @@ if [[ ! -d $INCREMENTALDIR ]];then
 fi
 
 # 가장 최신의 fullbackup 디렉토리 찾기
-ls -ltr | grep -v grep | awk '{print $9}' | grep -v ^$ |head -1
+ls -ltr | grep -v grep | awk '{print $9}' | grep -v ^$ |tail -1
 # 4. INCREMENTAL Restore
 # ex) mysqlbackup --incremental-backup-dir=/2017-02-22_10-12-41 --backup-dir=/2017-02-22_10-02-36 apply-incremental-backup
 # $MYSQLBACKUP --incremental-backup-dir=$BACKUPDIR/2020-10-07_14-56-14 --backup-dir=$BACKUPDIR/2020-10-07_13-23-56 apply-incremental-backup
