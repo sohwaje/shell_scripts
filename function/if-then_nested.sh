27cc6182d2fe282ab7134cabef7ba49820b8f9e9
Lee Yusung <yusunglee@Leeui-iMac.local>
2020-11-04 20:02:39 +0900

    add example2

--- function/if-then_nested.sh
+++ function/if-then_nested.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+# example 1
 if [[ conditino1 ]]
 then
   if [[ condition2 ]]
   then
     do-something  # "conditino1"과 "condition2"가 모두 참일 경우에만
   fi
+fi
+
+# example 2
+if [[ conditino1 ]] && [[ condition2 ]]
+then
+  do-something
 fi
