ec170d038439d745dd4d53b6ce6619f2bebcdfbd
sohwaje <kissenyo@gmail.com>
2022-02-04 10:37:04 +0900

    Update LSI_sas2008_1.sh

--- LSI_sas2008_1.sh
+++ LSI_sas2008_1.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
+check=$(sas2ircu 0 display |grep State |grep -v Optimal |grep -v Rebuilding |wc -l)
 sas2ircu 0 display |grep 'State'|awk -F ':' '{print $2}' >> /tmp/status.txt
 sas2ircu 0 display |grep 'Slot #'|awk -F ':' '{print $2}' >> /tmp/hdd_no.txt
 paste /tmp/status.txt /tmp/hdd_no.txt >> /tmp/disk_info.txt
  
  
-INDEX=0
-cat /tmp/disk_info.txt |awk '{print $1}'| while read line; do
- if [[ "$line" == "Optimal" ]];then
- msg="\" $(hostname): DISK $INDEX is OK"\"
- echo "${msg}"
+cat /tmp/disk_info.txt| while read line; do
+ if [[ "$check" == "0" ]];then
+ echo "ALL OK"
  else
- msg="\" $(hostname): DISK $INDEX was Failed"\"
- echo "${msg}"
+ # $line 변수의 $1 필드 값이 "Optimal"이 아니면 "Not OK:" 메시지와 함께 $3필드를 출력
+ msg=$(echo $line |awk '$1 != "Optimal" { print "Not OK:" $3 }')
+ echo $msg
  fi
- INDEX=`expr $INDEX + 1`
 done
  
  
