16dc0c0f5b9b21203810348de99d0e663e70e2db
sohwaje <kissenyo@gmail.com>
2022-02-14 13:27:52 +0900

    Update disk_UUID_chk.sh

--- check/UUID_CHECK/disk_UUID_chk.sh
+++ check/UUID_CHECK/disk_UUID_chk.sh
@@ -8,38 +8,47 @@
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 RESET=$(tput sgr0)
- 
-# 디바이스, 마운트 포인트, UUID를 /root/old_fs.txt에 저장한다.
-if [[ ! -e /root/old_fs.txt ]];then
- lsblk -o NAME,MOUNTPOINT,UUID | \
+
+# 재부팅 전 파일시스템 정보 파일과 재부팅 후 파일 시스템 정보 파일 저장 경로
+OLD_FILE_SYSTEM="/root/old_fs.txt"
+NEW_FILE_SYSTEM="/root/new_fs.txt"
+
+disk_info()
+{
+lsblk -o NAME,MOUNTPOINT,UUID | \
  grep -E "sd|xv" | \                # 물리 디스크 명
  grep -E "/|SWAP" | \               # 파티셔닝 된 파일 시스템은 "/"로 시작하고, 스왑은 "SWAP"로 나타난다.
  sed 's/[^a-z,A-Z,0-9,/]/ /g' | \   # 모든 특수문자를 공백으로 대체하지만, 단 "/"는 파티션을 나타내므로 대체하지 않는다.( /, /boot, /data, /home 등)
- awk '{print $1"="$2"="$3"-"$4"-"$5"-"$6"-"$7}' > old_fs.txt  # UUID의 공백은 다시 "-"로 대체한다.
+ awk '{print $1"="$2"="$3"-"$4"-"$5"-"$6"-"$7}'
+}
+
+# 디바이스, 마운트 포인트, UUID를 /root/old_fs.txt에 저장한다.
+old_fs_()
+{
+if [[ ! -e "$1" ]];then
+ disk_info > "$1"  # UUID의 공백은 다시 "-"로 대체한다.
 fi
- 
-# 재부팅 전 파일시스템의 디바이스, 마운트 포인트, UUID를 배열로 저장
 old_fs=()
-for oldfs in $(cat /root/old_fs.txt);do
+for oldfs in $(cat "$1");do
  old_fs+=("$oldfs")
 done
  # echo ${old_fs[@]} # all value = device
  # echo ${!old_fs[@]} # all Key = UUID
- 
-# 재부팅 후 파일시스템의 디바이스, 마운트 포인트, UUID
-lsblk -o NAME,MOUNTPOINT,UUID | \
- grep -E "sd|xv" | \
- grep -E "/|SWAP" | \
- sed 's/[^a-z,A-Z,0-9,/]/ /g' | \
- awk '{print $1"="$2"="$3"-"$4"-"$5"-"$6"-"$7}' > /root/new_fs.txt
-# 재부팅 후 파일시스템의 디바이스, 마운트 포인트, UUID를 배열로 저장
+}
+
+new_fs_()
+{
+disk_info > "$1"
 new_fs=()
-for newfs in $(cat /root/new_fs.txt);do
+for newfs in $(cat "$1");do
  new_fs+=("$newfs") 
 done
  # echo ${new_arrs[@]} # all value = device
  # echo ${!new_arrs[@]} # all Key = UUID
- 
+}
+
+print_result()
+{
 len=${#old_fs[*]} # 배열 전체 길이 구하기
 printf "=================================================================================================================\n"
 printf "%20s %38s %30s\n" OLD RESULT NEW
@@ -57,3 +66,8 @@ do
  fi
 done
 printf "=================================================================================================================\n"
+}
+
+old_fs_ $OLD_FILE_SYSTEM
+new_fs_ $NEW_FILE_SYSTEM
+print_result
