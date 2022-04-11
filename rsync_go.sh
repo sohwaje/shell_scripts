86f815e191d56217ec7c7976f1aaf40ce113a1c1
sohwaje <kissenyo@gmail.com>
2022-04-11 10:50:51 +0900

    update

--- rsync_go.sh
+++ rsync_go.sh
@@ -11,81 +11,81 @@
 print_help()
 {
  # 사용법: ./rsync_go.sh -s <Src dir> -d <데이터를 쏠 IP>
- echo "usage : $0 -s <Src dir> -d <Dst Ipaddress>"
- exit 1
+ echo "usage : $0 -s <Src dir> -d <Dst Ipaddress>"
+ exit 1
 }
- 
+
 ## 인자값이 3개 미만이면 print_help 함수 호출하고 프로그램 종료
 if [[ $# -lt 2 ]]; then
- print_help
+ print_help
 fi
- 
+
 ## Main task code
 while getopts s:d:h opt
 do
- case $opt in
- s)
- S=$OPTARG;;
- d)
- D=$OPTARG;;
- h)
- print_help;;
- *)
- print_help;;
- esac
+case $opt in
+ s)
+ S=$OPTARG;;
+ d)
+ D=$OPTARG;;
+ h)
+ print_help;;
+ *)
+ print_help;;
+ esac
 done
- 
+
 if [[ -z ${S}] || -z ${D} ]]; then
- echo "Invalid arguments"
- print_help
+ echo "Invalid arguments"
+ print_help
 fi
- 
+ 
 ## 소스 디렉토리 마지막에 "/"로 끝나는지 확인해서 없으면 추가
 if [[ $(echo ${S:(-1)}) == "/" ]]; then
- SRC_DIR=${S}
+ SRC_DIR=${S}
 else
- SRC_DIR=${S}/
+ SRC_DIR=${S}/
 fi
- 
+
 ## 스크립트 실행 시작 시간
 StartTime=$(date +%s)
- 
+
 ## 멀티스레드 방식으로 스크립트를 실행시키는 옵션
 put_multi_thread_rsync()
 {
- echo "ls -d ${dir} | xargs -I {} -P 5 -n 1 rsync ${OPTION} --size-only {} ${D}::R${dir}"
- ls -d ${dir} | xargs -I {} -P 5 -n 1 rsync ${OPTION} --size-only {} ${D}::R${dir}
+ echo "ls -d ${dir} | xargs -I {} -P 5 -n 1 rsync ${OPTION} --size-only {} ${D}::R${dir}"
+ ls -d ${dir} | xargs -I {} -P 5 -n 1 rsync ${OPTION} --size-only {} ${D}::R${dir}
 }
- 
+
 rsync_go()
 {
 ## 0 depth : 최초 0depth 디렉토리 내용을 가져옴
 for i in $(ls -d ${SRC_DIR} 2> /dev/null); do
- echo "0 depth"
- dir=${i}
- OPTION="-lptgoDqd"
- put_multi_thread_rsync
- 
+ echo "0 depth"
+ dir=${i}
+ OPTION="-lptgoDqd"
+ put_multi_thread_rsync
+
     ## 1 depth : 1depth 디렉토리 내용을 가져옴
-    for j in $(ls -d ${i}*/ 2> /dev/null); do
-      echo "1 depth #"
-      dir=${j}
-      OPTION="-lptgoDqd"
-      put_multi_thread_rsync
- 
+   for j in $(ls -d ${i}*/ 2> /dev/null); do
+     echo "1 depth #"
+     dir=${j}
+     OPTION="-lptgoDqd"
+     put_multi_thread_rsync
+
         ## 2 depth : 2depth 이하는 -av로 모두 가져옴
-        for k in $(ls -d ${j}*/ 2> /dev/null); do
-          echo "2 depth ##"
-          dir=${k}
-          OPTION="-av"
-          put_multi_thread_rsync
-        done
-    done
+       for k in $(ls -d ${j}*/ 2> /dev/null); do
+         echo "2 depth ##"
+         dir=${k}
+         OPTION="-av"
+         put_multi_thread_rsync
+       done
+   done
 done
 }
- 
+
 rsync_go
- 
+
 EndTime=$(date +%s)
 echo "It takes $(($EndTime - $StartTime)) seconds to complete this task."
 
